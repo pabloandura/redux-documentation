50c04dc04adeb66178c5b79483c31bbc090fd7b1
pabloandura <pablo.andura@gmail.com>
2022-08-04 14:57:46 -0300

    first-commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from '@reduxjs/toolkit'
 
-export const store = configureStore({
+import postsReducer from '../features/posts/postsSlice'
+import usersReducer from '../features/users/usersSlice'
+
+export default configureStore({
   reducer: {
-    counter: counterReducer,
+    posts: postsReducer,
+    users: usersReducer,
   },
-});
+})
